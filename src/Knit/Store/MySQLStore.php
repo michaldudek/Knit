d425e029d80ca2ad8ab2646ca557451a01c5bda9
Michał Dudek <michal@michaldudek.pl>
2013-04-26 01:47:33 +0200

    MySQLStore now uses PDO to connect to the database. Partial implementation of executing queries logging them. Removed unnecessary dependency on Splot Log.

--- src/Knit/Store/MySQLStore.php
+++ src/Knit/Store/MySQLStore.php
@@ -8,16 +8,19 @@
  * 
  * @copyright Copyright (c) 2013, Michał Dudek
  * @license MIT
- * 
- * @todo rewrite to PDO/mysqli
  */
 namespace Knit\Store;
+
+use PDO;
+use PDOException;
+use PDOStatement;
 
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 
 use MD\Foundation\Exceptions\NotFoundException;
 use MD\Foundation\Utils\ArrayUtils;
+use MD\Foundation\Utils\StringUtils;
 
 use Knit\Exceptions\StoreConnectionFailedException;
 use Knit\Exceptions\StoreQueryErrorException;
@@ -63,6 +66,30 @@ class MySQLStore implements StoreInterface
      * STORE INTERFACE IMPLEMENTATION
      *****************************************************/
     public function find($table, array $criteria = array(), array $params = array()) {
+        $criteria = $this->parseCriteria($criteria);
+
+        throw new \RuntimeException('@TODO: MySQLStore::find()');
+
+        /*
+        
+        // parse any optional params
+        $orderBy        = isset($params['orderBy'])     ? $this->makeSafe($params['orderBy'])   : false;
+        $orderDir       = ((isset($params['orderDir'])) AND in_array($params['orderDir'], array(MDOrderAscending, MDOrderDescending))) ? $params['orderDir'] : MDOrderAscending;
+        $start          = isset($params['start'])       ? intval($params['start'])              : 0;
+        $limit          = isset($params['limit'])       ? intval($params['limit'])              : false;
+        $forceMulti     = ($limit == 1)                 ? false                                 : true;
+        
+        $query = "
+            SELECT
+                *
+            FROM `". $this->makeSafe($table) ."`
+            ". ($criteria           ? "WHERE ". $criteria                                   : null) ."
+            ". ($orderBy            ? "ORDER BY `". $orderBy ."` ". $orderDir               : null) ."
+            ". ($limit              ? "LIMIT ". $start .", ". $limit                        : null) ."
+        ";
+
+        return $this->query($query, $forceMulti);
+        */
         return array();
     }
 
@@ -75,11 +102,9 @@ class MySQLStore implements StoreInterface
     }
 
     public function update($table, array $criteria, array $values) {
-
     }
 
     public function delete($table, array $criteria) {
-
     }
 
     /**
@@ -89,10 +114,9 @@ class MySQLStore implements StoreInterface
      * @return array
      */
     public function structure($table) {
-        $query = "SHOW COLUMNS FROM `". $this->makeSafe($table) ."`";
-        $columns = $this->query($query, true);
-        $structure = array();
+        $columns = $this->query('SHOW COLUMNS FROM '. $table, array(), true);
 
+        $structure = array();
         foreach($columns as $column) {
             // resolve the MySQL type to Knit type
             $type = self::mysqlToKnitType($column['Type']);
@@ -119,117 +143,112 @@ class MySQLStore implements StoreInterface
      * 
      * @throws \RuntimeException When already connected and trying to connect for 2nd time.
      * @throws StoreConnectionFailedException When failled to connect to the database.
-     * @throws NotFoundException When couldn't find the database.
      */
     protected function connect() {
         if ($this->connection) {
             throw new \RuntimeException("Already connected to MySQL database. Cannot connect twice. If you want to connect to a different database you need to instantiate a new MySQLStore.");
         }
 
-        $connection = mysql_connect($this->hostname, $this->username, $this->password);
-        // if failed to establish connection then trigger an error
-        if (!$connection) {
-            throw new StoreConnectionFailedException('Cannot connect to MySQL database "'. $this->username .' @ '. $this->hostname .'".');
+        try {
+            $this->connection = new PDO('mysql:host='. $this->hostname .';dbname='. $this->database, $this->username, $this->password);
+            // make it throw exceptions on errors
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            throw new StoreConnectionFailedException($e->getMessage(), 0, $e);
         }
-
-        // store reference to the connection in the class
-        $this->connection = $connection;
         
         // set utf8 character set for all requests
-        $this->forceUtf8();
-        
-        // select a database
-        $selected = mysql_select_db($this->database, $this->connection);
-        if (!$selected) {
-            throw new NotFoundException('Cannot connect to database "'. $this->database .' @ '. $this->hostname .'". No such database exists.');
-        }
+        $this->connection->exec("SET CHARACTER SET utf8");
     }
 
     /**
-     * Executes the given SQL query.
+     * Executes the given PDO statement (SQL query).
      * 
      * @param string $sql SQL query to be executed.
+     * @param array $params [optional] Array of parameters that should be bound to the SQL query.
      * @param bool $forceMulti [optional] Should the result be always a collection (array of results)? Default: false.
      * @return array Data returned from the database as an array.
+     * 
+     * @throws StoreQueryErrorException When there was an error executing the query.
      */
-    public function query($sql, $forceMulti = false) {
+    protected function query($sql, array $params = array(), $forceMulti = false) {
         // lazy connection, only when needed
         if (!$this->isConnected()) {
             $this->connect();
         }
         
-        // execute the query
-        $sql = trim($sql);
-        $result = mysql_query($sql, $this->connection);
-        
-        // handle the query's error
-        if ($result === false) {
-            throw new StoreQueryErrorException('MySQL: '. mysql_error($this->connection), mysql_errno($this->connection));
+        try {
+            $statement = $this->connection->prepare(trim($sql));
+            $statement->execute($params);
+        } catch (PDOException $e) {
+            $this->logQuery($statement, array(
+                'params' => $params
+            ), true);
+            throw new StoreQueryErrorException('MySQL: '. $e->getMessage(), 0, $e);
         }
-        
+
+        $type = $this->determineQueryType($statement->queryString);
+        $rowCount = $statement->rowCount();
+
         // initial parsing of the result
         // only SELECT and SHOW queries can return multiple rows
         $data = array();
-        if (stripos($sql, 'SELECT') === 0 OR stripos($sql, 'SHOW') === 0) {
-            if ($forceMulti OR (@mysql_num_rows($result) > 1)) {
-                while($row = @mysql_fetch_array($result, MYSQL_ASSOC)) {
+        if ($type == 'select' || $type == 'show') {
+            if ($forceMulti || $rowCount > 1) {
+                while($row = $statement->fetch(PDO::FETCH_ASSOC)) {
                     $data[] = $row;
                 }
-            } elseif (@mysql_num_rows($result) === 1) {
-                $data = @mysql_fetch_array($result, MYSQL_ASSOC);
+            } else {
+                $data = $statement->fetch(PDO::FETCH_ASSOC);
             }
         }
-        
-        // log the query
-        // @todo
-        //$affectedRows = mysql_affected_rows($this->connection);
-        //$this->logQuery($sql, $time, $affectedRows);
 
-        // free the result once we have it
-        if (is_resource($result)) {
-            @mysql_free_result($result);
-        }
-        
+        // finally log the query
+        $this->logQuery($statement, array(
+            'type' => $type,
+            'rows' => $rowCount
+        ));
+
         return $data;
-    }
-
-    /**
-     * Sets UTF8 character set for all queries.
-     */
-    public function forceUtf8() {
-        mysql_query("SET CHARACTER SET utf8", $this->connection);
-        mysql_query("SET NAMES utf8", $this->connection);
     }
 
     /*****************************************************
      * HELPERS
      *****************************************************/
     /**
-     * Makes the value safe to be used inside an SQL Query
-     *
-     * @param mixed $value Can be an array.
-     * @return mixed
+     * Logs the given PDO statement (database query) to the available logger.
+     * 
+     * @param PDOStatement $statement Query to be logged.
+     * @param array $context [optional] Context of the query (like execution time) that can't be read from the PDOStatement.
+     * @param bool $error [optional] Has an error occurred on this query? Default: false.
+     * 
+     * @todo Improve logging of the queries by:
+     *       - adding tags,
+     *       - adding execution time,
+     *       - adding trace or trying to figure out where it was executed.
      */
-    public function makeSafe($value) {
-        // MySQL connection is required otherwise the mysql_real_escape_string() will fail..
-        if (!$this->isConnected()) {
-            $this->connect();
-        }
-        
-        // go recursively through array
-        if (is_array($value)) {
-            foreach($value as &$val) {
-                $val = $this->makeSafe($val);
-            }
-            return $value;
-        }
-        
-        // don't do this for ints which are safe anyway, but this makes them string which may break stuff down the line
-        if (!is_int($value)) {
-            $value = mysql_real_escape_string($value, $this->connection);
-        }
+    public function logQuery(PDOStatement $statement, array $context = array(), $error = false) {
+        $message = $statement->queryString;
+        $context = array_merge($context, array(
+            'query' => $statement->queryString
+        ));
 
-        return $value;
+        if ($error) {
+            $this->logger->error($message, $context);
+        } else {
+            $this->logger->info($message, $context);
+        }
+    }
+
+    /**
+     * Determines query type based on the SQL string.
+     * 
+     * @param string $sql SQL query.
+     * @return string
+     */
+    public function determineQueryType($sql) {
+        $type = StringUtils::getFirstWord($sql);
+        return strtolower($type);
     }
 
     /**
@@ -240,7 +259,7 @@ class MySQLStore implements StoreInterface
      * 
      * @throws \RuntimeException When could not resolve MySQL type to Knit type.
      */
-    public static function mysqlToKnitType($type) {
+    protected static function mysqlToKnitType($type) {
         $type = trim($type);
         $name = null;
         $length = 0;
@@ -286,6 +305,10 @@ class MySQLStore implements StoreInterface
             'length' => $length,
             'allowed' => $allowed
         );
+    }
+
+    protected function parseCriteria(array $criteria) {
+        throw new \RuntimeException('@TODO: MySQLStore::parseCriteria()');
     }
 
     /*****************************************************
